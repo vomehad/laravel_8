179b8e93ca04230af138fb406b8fca015fed04f3
vomehad <vomehad@yandex.ru>
2022-01-16 20:06:34 +0300

    use header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!doctype html>
 <html lang="en">
 <head>
-    @yield('meta')
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    >
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @yield('meta')
     <title>@yield('title')</title>
     @include('includes.assets.css')
 </head>

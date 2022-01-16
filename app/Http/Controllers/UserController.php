0dfcd78c705656d640839c53e004b3fb6ab8e3de
vomehad <vomehad@yandex.ru>
2022-01-16 19:08:05 +0300

    change view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,10 +6,15 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function getAll()
+    public function welcome()
     {
         $users = User::all();
 
         return view('welcome', ['users' => $users]);
+    }
+
+    public function home()
+    {
+        return view('home');
     }
 }

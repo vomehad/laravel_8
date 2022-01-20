0214424227b161ac69a9b38c97b206961e768558
vomehad <vomehad@yandex.ru>
2022-01-20 21:59:48 +0300

    delete preprocessor

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/css/scss/app.scss', 'public/css')
+    // .sass('resources/css/scss/app.scss', 'public/css')
     .postCss('resources/css/app.css', 'public/css', []);

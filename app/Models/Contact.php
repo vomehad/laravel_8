ce7ed53917f83d8e945026c89bda80b4bbdefcdc
d.mozhaev <d.mozhaev@digital-sector.ru>
2022-01-21 15:37:25 +0300

    create correct models

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+
+    protected $table = 'contacts';
+
+    public static function tableName(): string
+    {
+        return 'contacts';
+    }
+
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'name',
+        'email',
+        'subject',
+        'message',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast
+     *
+     * @var array
+     */
+    protected $casts = [];
 }

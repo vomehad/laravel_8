83f5c95b709692930a0cbe8ea6ca870f6e802348
d.mozhaev <d.mozhaev@digital-sector.ru>
2022-01-21 11:35:10 +0300

    Add Logo

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,10 +1,8 @@
 <header class="header-content">
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        @if(isset($title))
-            <a href="<?= route('Home') ?>" class="my-0 mr-md-auto font-weight-normal text-decoration-none">
-                <span class="fs-4">{{ $title }}</span>
-            </a>
-        @endif
+        <a href="<?= route('Home') ?>" class="my-0 mr-md-auto font-weight-normal text-decoration-none">
+            <span class="fs-4">Vomehad</span>
+        </a>
 
         @if(isset($nav) && !empty($nav))
             <nav class="my-2 my-md-0 mr-md-3 nav-content">

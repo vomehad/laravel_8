d7588f59863b1b6b7ee053dc31d79087cc158688
d.mozhaev <d.mozhaev@digital-sector.ru>
2022-01-27 12:36:10 +0300

    fix game

--- resources/views/playGame.blade.php
+++ resources/views/playGame.blade.php
@@ -20,7 +20,9 @@
                 @endfor
                 </table>
 
-                <button id="start" class="btn btn-block" onclick="readyToGame()">Start</button>
+                <button id="start"
+                        class="btn btn-block"
+                >Start</button>
 
                 <p id="timer"></p>
 
@@ -32,206 +34,210 @@
             </div>
         </div>
         <script>
-            function readyToGame() {
-                const start = document.querySelector('#start');
-                const newGame = document.querySelector('#theEnd');
+            const start = document.querySelector('#start');
+            const newGame = document.querySelector('#theEnd');
 
-                start.addEventListener('click', startGame);
-                newGame.addEventListener("click", goNewGame);
+            start.addEventListener('click', startGame);
+            newGame.addEventListener("click", goNewGame);
 
-                function startGame() {
-                    const divGame = document.querySelector('#game');
-                    const tds = divGame.querySelectorAll('td');
-                    const timer = divGame.querySelector('#timer');
-                    const colors = fillArray();
-                    const obj = createObj();
-                    let arrTd = [];
+            function startGame() {
+                const divGame = document.querySelector('#game');
+                const tds = divGame.querySelectorAll('td');
+                const timer = divGame.querySelector('#timer');
+                const colors = fillArray();
+                const obj = createObj();
+                let arrTd = clearArray();
 
-                    start.disabled = true;
-                    timerGame();
+                disable(start);
+                timerGame();
 
-                    for (let td of tds) {
-                        td.classList.add('active');
-                        td.addEventListener('click', clickMode);
-                    }
+                for (let td of tds) {
+                    td.classList.add('active');
+                    td.addEventListener('click', clickMode);
+                }
 
-                    function clickMode() {
-                        this.style.background = obj[this.innerHTML];
-                        arrTd.push(this);
-                        matchTd(arrTd);
+                function clickMode() {
+                    this.style.background = obj[this.innerHTML];
+                    arrTd.push(this);
+                    matchTd(arrTd);
 
-                        if (arrTd.length === 2 && arrTd[0].style.background !== arrTd[1].style.background) {
+                    if (arrTd.length === 2) {
+                        if (arrTd[0].style.background !== arrTd[1].style.background) {
                             notMatch();
                         } else if (arrTd[0].style.background === arrTd[1].style.background) {
                             getMatch();
                         }
                     }
-
-                    function getMatch() {
-                        for (let td of tds) {
-                            td.removeEventListener('click', clickMode);
-                        }
-
-                        const id = setTimeout(function() {
-                            arrTd[0].style.background = 'white';
-                            arrTd[1].style.background = 'white';
-                            arrTd = [];
-
-                            for (let td of tds) {
-                                if (td.classList.contains('active')) {
-                                    td.addEventListener('click', )
-                                }
-                            }
-
-                            clearTimeout(id);
-                        }, 300);
-                    }
-
-                    function notMatch() {
-                        for (let td of tds) {
-                            td.removeEventListener('click', clickMode);
-                        }
-
-                        const id = setTimeout(function() {
-                            arrTd[0].style.background = 'white';
-                            arrTd[1].style.background = 'white';
-                            arrTd = [];
-
-                            for (let td of tds) {
-                                if (td.classList.contains('active')) {
-                                    td.addEventListener('click', clickMode);
-                                }
-                            }
-
-                            clearTimeout(id);
-                        }, 300);
-                    }
-
-                    function createObj() {
-                        const obj = {};
-                        const set = new Set();
-
-                        while (set.size !== 16) {
-                            set.add(Math.ceil(Math.random() * 16));
-                        }
-
-                        newObj(set, obj);
-
-                        return obj;
-                    }
-
-                    function newObj(arr, obj) {
-                        let i = 0;
-
-                        arr.forEach((elem) => {
-                            i++;
-                            obj[i] = colors[elem];
-                        })
-                    }
-
-                    function matchTd(arr) {
-                        checkEqual(arr);
-
-                        if (arr.length > 2) {
-                            arr.splice(2);
-                        }
-                    }
-
-                    function checkEqual(arr) {
-                        if (arr[0] === arr[1]) {
-                            arr.splice(1);
-                        }
-                    }
-
-                    function Win() {
-                        if (divGame.querySelectorAll('.active').length > 0) {
-                            return true;
-                        }
-                    }
-
-                    // let Win = () => divGame.querySelectorAll('.active').length;
-
-                    function timerGame() {
-                        timer.innerHTML = "00:00:000";
-
-                        let millisec = 0;
-                        let seconds = 0;
-                        let minutes = 0;
-                        let time = new Date().getTime();
-
-                        const id = setInterval(function() {
-                            millisec = new Date().getTime() - time;
-
-                            if (millisec > 999) {
-                                time = new Date().getTime();
-                                seconds++;
-                            }
-
-                            if (seconds > 59) {
-                                seconds = 0;
-                                minutes++;
-                            }
-
-                            if (Win()) {
-                                clearInterval(id);
-                                modalWindow();
-                            }
-
-                            timer.innerHTML = getZero(minutes) + ":" + getZero(seconds) + "." + millisec;
-                            // timer.innerHTML = `${getZero(minutes)}:${getZero(seconds)}.${millisec};
-                        }, 1);
-                    }
-
-                    function getZero(num) {
-                        if (num <= 9) {
-                            return '0' + num;
-                        } else {
-                            return num;
-                        }
-                    }
-                    // const getZero = (num) => num < 10 ? `0${num}` : num;
-
-                    function modalWindow() {
-                        document.querySelector('#winner').style.display = 'grid';
-                        document.querySelector('#yourTime').innerHTML = 'Time spend: ' + timer.innerHTML;
-                    }
                 }
 
-                function fillArray() {
-                    return [
-                        '1',
-                        'red',
-                        'red',
-                        'green',
-                        'green',
-                        'blue',
-                        'blue',
-                        'black',
-                        'black',
-                        'yellow',
-                        'yellow',
-                        'hotpink',
-                        'hotpink',
-                        'indigo',
-                        'indigo',
-                        'magenta',
-                        'magenta',
-                    ];
-                }
+                function getMatch() {
+                    for (let td of tds) {
+                        td.removeEventListener('click', clickMode);
+                    }
 
-                function goNewGame() {
-                    const divGame = document.querySelector('#game');
-                    const timer = divGame.querySelector('#timer');
-                    const tds = divGame.querySelectorAll('td');
-
-                    timer.innerHTML = "00:00.000";
-                    document.querySelector('#winner').style.display = 'none';
-                    start.disabled = false;
+                    arrTd[0].classList.remove('active');
+                    arrTd[1].classList.remove('active');
 
                     for (let td of tds) {
-                        td.style.background = 'white';
+                        if (td.classList.contains('active')) {
+                            td.addEventListener('click', clickMode)
+                        }
+                    }
+
+                    arrTd = clearArray();
+                }
+
+                function notMatch() {
+                    for (let td of tds) {
+                        td.removeEventListener('click', clickMode);
+                    }
+
+                    const id = setTimeout(function() {
+                        arrTd[0].style.background = 'white';
+                        arrTd[1].style.background = 'white';
+                        arrTd = [];
+
+                        for (let td of tds) {
+                            if (td.classList.contains('active')) {
+                                td.addEventListener('click', clickMode);
+                            }
+                        }
+
+                        clearTimeout(id);
+                    }, 300);
+                }
+
+                function createObj() {
+                    const obj = {};
+                    const set = new Set();
+
+                    while (set.size !== 16) {
+                        set.add(Math.ceil(Math.random() * 16));
+                    }
+
+                    newObj(set, obj);
+
+                    return obj;
+                }
+
+                function newObj(arr, obj) {
+                    let i = 0;
+
+                    arr.forEach((elem) => {
+                        i++;
+                        obj[i] = colors[elem];
+                    })
+                }
+
+                function matchTd(arr) {
+                    checkEqual(arr);
+
+                    if (arr.length > 2) {
+                        arr.splice(2);
                     }
                 }
+
+                function checkEqual(arr) {
+                    if (arr[0] === arr[1]) {
+                        arr.splice(1);
+                    }
+                }
+
+                function Win() {
+                    if (divGame.querySelectorAll('.active').length === 0) {
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                // let Win = () => divGame.querySelectorAll('.active').length;
+
+                function timerGame() {
+                    timer.innerHTML = "00:00:000";
+
+                    let millisec = 0;
+                    let seconds = 0;
+                    let minutes = 0;
+                    let time = new Date().getTime();
+
+                    const id = setInterval(function() {
+                        millisec = new Date().getTime() - time;
+
+                        if (millisec > 999) {
+                            time = new Date().getTime();
+                            seconds++;
+                        }
+
+                        if (seconds > 59) {
+                            seconds = 0;
+                            minutes++;
+                        }
+
+                        if (Win()) {
+                            clearInterval(id);
+                            modalWindow();
+                        }
+
+                        timer.innerHTML = getZero(minutes) + ":" + getZero(seconds) + "." + millisec;
+                        // timer.innerHTML = `${getZero(minutes)}:${getZero(seconds)}.${millisec};
+                    }, 1);
+                }
+
+                function getZero(num) {
+                    if (num <= 9) {
+                        return '0' + num;
+                    } else {
+                        return num;
+                    }
+                }
+                // const getZero = (num) => num < 10 ? `0${num}` : num;
+
+                function modalWindow() {
+                    document.querySelector('#winner').style.display = 'grid';
+                    document.querySelector('#yourTime').innerHTML = 'Time spend: ' + timer.innerHTML;
+                }
+            }
+
+            function fillArray() {
+                return [
+                    '1',
+                    'red',
+                    'red',
+                    'green',
+                    'green',
+                    'blue',
+                    'blue',
+                    'black',
+                    'black',
+                    'yellow',
+                    'yellow',
+                    'hotpink',
+                    'hotpink',
+                    'indigo',
+                    'indigo',
+                    'magenta',
+                    'magenta',
+                ];
+            }
+
+            function goNewGame() {
+                const divGame = document.querySelector('#game');
+                const timer = divGame.querySelector('#timer');
+                const tds = divGame.querySelectorAll('td');
+
+                timer.innerHTML = "00:00.000";
+                document.querySelector('#winner').style.display = 'none';
+                start.disabled = false;
+
+                for (let td of tds) {
+                    td.style.background = 'white';
+                }
+            }
+
+            const clearArray = () => [];
+            const disable = (elem) => {
+                elem.disabled = true;
             }
         </script>
     @endsection

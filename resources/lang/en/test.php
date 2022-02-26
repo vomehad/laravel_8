d5bca4351a11ff1dca717d821150635605157a1c
Den <vomehad@yandex.ru>
2022-02-26 17:17:46 +0300

    translations files

--- resources/lang/en/test.php
+++ resources/lang/en/test.php
@@ -6,9 +6,14 @@ return [
             'hourly' => 'input to set by the hour',
             'forever' => 'input to set forever',
         ],
+        'button' => 'Send',
     ],
     'word' => [
         'placeholder' => 'enter the word for split',
         'button' => 'Split',
-    ]
+    ],
+    'text' => [
+        'placeholder' => 'Text with dates',
+        'button' => 'Send'
+    ],
 ];

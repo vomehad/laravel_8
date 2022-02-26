ebfed9d492dedc0d56a4b8198662e71068e07899
Den <vomehad@yandex.ru>
2022-02-26 18:25:38 +0300

    add columns

--- database/migrations/2022_02_26_151550_create_notes_table.php
+++ database/migrations/2022_02_26_151550_create_notes_table.php
@@ -15,6 +15,8 @@ class CreateNotesTable extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->tinyText('name')->nullable(false);
+            $table->text('content')->nullable(true);
             $table->timestamps();
         });
     }

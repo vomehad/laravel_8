b50feeea1cf6a3f1855ec031b0b5b51274d12a04
Den <vomehad@yandex.ru>
2022-05-01 19:26:29 +0300

    fix header

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -1,6 +1,6 @@
 <header class="header-content">
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <a href="<?= route('Home') ?>" class="my-0 mr-md-auto font-weight-normal text-decoration-none">
+        <a href="<?= route('home') ?>" class="my-0 mr-md-auto font-weight-normal text-decoration-none">
             <span class="fs-4">Vomehad</span>
         </a>
         {{--        <div class="col">--}}
@@ -23,12 +23,9 @@
                 <a href="{{ route('Logout') }}" class="btn btn-outline-prim">Logout</a>
             @endauth
         </nav>
-
     </div>
 
     <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-
             <h1 class="display-4 fw-normal">{{ $title ?? __('Titles' . '.' . \App\Helpers\Helper::getActionName()) }}</h1>
-
     </div>
 </header>
